961076f62d63611518742f5d90ca1f3c593fbb0a
Alexander Kozhenkin <chenzeyroyal@gmail.com>
2025-10-30 20:57:27 +0300

    Deploy tries

--- minista.config.js
+++ minista.config.js
@@ -3,7 +3,7 @@ import path from "path"
 
 export default defineConfig({
   root: "",
-  base: process.env.NODE_ENV === "production" ? "/lumen/" : "/",
+  base: "/lumen/",
   public: "public",
   out: "dist",
   assets: {
